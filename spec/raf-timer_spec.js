850b68e1cca8ede713ea166d4f631ac8bc3a5c87
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-10-26 10:47:40 -0400

    instantiation test

--- spec/raf-timer_spec.js
+++ spec/raf-timer_spec.js
@@ -5,4 +5,10 @@ describe('RAFTimer', function(){
   it('exists', function(){
     expect(RAFTimer).toExist();
   });
+
+  it('can be intantiated', function(){
+    const instance = new RAFTimer();
+    expect(instance).toExist();
+    expect(instance).toBeA(RAFTimer);
+  });
 });
