0c6eb9329a80aef88beff0fe38892f716df8a68a
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-10-26 10:47:20 -0400

    fix test typo

--- spec/raf-timer_spec.js
+++ spec/raf-timer_spec.js
@@ -1,7 +1,7 @@
 import expect from 'expect';
 import RAFTimer from '../src/index';
 
-define('RAFTimer', function(){
+describe('RAFTimer', function(){
   it('exists', function(){
     expect(RAFTimer).toExist();
   });
